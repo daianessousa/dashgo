3793a39fffc2566ac9869c37b9e0c3c819834b71
Daiane Sousa <daisousa.c@gmail.com>
2021-09-18 22:19:01 -0300

    add pages login and dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
+import { Flex, Button, Stack} from "@chakra-ui/react";
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <div>
-     <h1> dashgo </h1>
-    </div>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail"/>
+          <Input name="password" type="password" label="senha"/>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
